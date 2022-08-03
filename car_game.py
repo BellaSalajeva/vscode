8236edc95293cbb716df515440bef43cdc68592e
Bella Salajeva <bella.salajeva@gmail.com>
2022-08-04 01:30:30 +0300

    Finished code for file

--- car_game.py
+++ car_game.py
@@ -1,9 +1,28 @@
 command = ""
+started = False
 
-while command.upper() != "quit":
-    if command.upper() == "help"
+while True:
+    command = input("> ").lower()
+    if command == "help":
         print('''
-        start - to start the car
-        stop - to stop the car
-        quite - to exit
+start - to start the car
+stop - to stop the car
+quit - to exit
         ''')
+    elif command == "start":
+        if started:
+            print("Car is already started")
+        else:
+            started = True
+            print("Car started...Ready to go!")
+    elif command == "stop":
+        if not started: # ie its stopped
+            print("Car is already stopped.")
+        else:
+            started = False
+            print("Car stopped.")
+    elif command == "quit":
+        break
+    else:
+        print("I don't understand that...")
+ 
