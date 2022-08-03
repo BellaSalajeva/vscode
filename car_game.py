36e067236e0dbaaa9b5a0f73fac07e4b4f600bc0
Bella Salajeva <bella.salajeva@gmail.com>
2022-08-03 18:59:07 +0300

    Added the car_game file

---
+++ car_game.py
@@ -0,0 +1,9 @@
+command = ""
+
+while command.upper() != "quit":
+    if command.upper() == "help"
+        print('''
+        start - to start the car
+        stop - to stop the car
+        quite - to exit
+        ''')
